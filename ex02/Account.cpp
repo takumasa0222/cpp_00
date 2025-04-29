443a72e60036d108646458929f04990b4fa3fe85
Takumasa Matsuura <tamatsuu@student.42tokyo.jp>
2025-04-29 23:42:24 +0900

    temp:create file for ex02

---
+++ ex02/Account.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tamatsuu <tamatsuu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 23:31:32 by tamatsuu          #+#    #+#             */
+/*   Updated: 2025/04/29 23:36:38 by tamatsuu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+int	Account::getNbAccounts( void )
+{
+	return Account::_nbAccounts;
+}
+
+int	Account::getTotalAmount( void )
+{
+	
+}
+
+int	Account::getNbDeposits( void )
+{
+	
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	
+}
+
+Account::Account (int initial_deposit)
+{
+	
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	
+}
+int		Account::checkAmount( void ) const
+{
+	
+}
+
+void	Account::displayStatus( void ) const
+{
+	
+}
+
+void	Account::_displayTimestamp( void )
+{
+	
+}
+
+Account::Account( void )
+{
+	
+}
